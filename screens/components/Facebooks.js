939c3488910b17109836d9edb2d7b874332f7556
VishalMishra0 <Vishal.mishra1@mail.vinove.com>
2022-07-09 09:56:51 +0530

    updated google signup

--- screens/components/Facebooks.js
+++ screens/components/Facebooks.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity,Image } from 'react-native';
-
-
 import * as firebase from 'firebase';
 import * as Facebook from 'expo-facebook';
 
--- screens/components/Googles.js
+++ screens/components/Googles.js
@@ -1,84 +1,121 @@
+
+// import * as React from 'react';
+// import * as WebBrowser from 'expo-web-browser';
+// import { ResponseType } from 'expo-auth-session';
+// import * as Google from 'expo-auth-session/providers/google';
+// import { initializeApp } from 'firebase/app';
+// import { getAuth, GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
+// import { Button } from 'react-native';
+// import firebase from "firebase";
+
+// // Initialize Firebase
+
+// const Config = {
+//   apiKey: "AIzaSyCflhxjfW0Kf1NG-T51i2LzJd7SSC47BaI",
+//   authDomain: "projectexpo-fce16.firebaseapp.com",
+//   databaseURL: "https://projectexpo-fce16-default-rtdb.firebaseio.com",
+//   projectId: "projectexpo-fce16",
+//   storageBucket: "projectexpo-fce16.appspot.com",
+//   messagingSenderId: "513477020718",
+//   appId: "1:513477020718:web:bee1c1ad733e7b9dbb23bf",
+//   measurementId: "G-QR3TBQN02D"
+// };
+
+// if (!firebase.apps.length) 
+//   // Your web app's Firebase configuration
+//   // Initialize Firebase
+//   firebase.initializeApp(Config);
+
+// WebBrowser.maybeCompleteAuthSession();
+
+// export default function App() {
+
+//   const [request, response, promptAsync] = Google.useIdTokenAuthRequest(
+//     {
+//       clientId: '513477020718-9l7r57nku76f7rm16fmbd4st590304ff.apps.googleusercontent.com',
+//       androidClientId:'513477020718-j9c5dqpq19e0ejb1vl4g5co085ik97mb.apps.googleusercontent.com'
+//       },
+//   );
+
+//   React.useEffect(() => {
+//     if (response?.type === 'success') {
+//       const { id_token } = response.params;
+      
+//       const auth = getAuth();
+//       const provider = new GoogleAuthProvider();
+//       const credential = provider.credential(id_token);
+//       signInWithCredential(auth, credential);
+//     }
+//   }, [response]);
+
+//   return (
+//     <Button
+//       disabled={!request}
+//       title="Login"
+//       onPress={() => {
+//         promptAsync();
+//         }}
+//     />
+//   );
+// }
+
+
+
 import * as React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet,Image } from 'react-native';
-import * as Google from 'expo-google-app-auth'
-import * as firebase from 'firebase';
+import * as WebBrowser from 'expo-web-browser';
+import { ResponseType } from 'expo-auth-session';
+import * as Google from 'expo-auth-session/providers/google';
+import firebase from 'firebase';
+import { Button } from 'react-native';
+
+
 
 const Config = {
-    apiKey: "AIzaSyCflhxjfW0Kf1NG-T51i2LzJd7SSC47BaI",
-    authDomain: "projectexpo-fce16.firebaseapp.com",
-    databaseURL: "https://projectexpo-fce16-default-rtdb.firebaseio.com",
-    projectId: "projectexpo-fce16",
-    storageBucket: "projectexpo-fce16.appspot.com",
-    messagingSenderId: "513477020718",
-    appId: "1:513477020718:web:bee1c1ad733e7b9dbb23bf",
-    measurementId: "G-QR3TBQN02D"
-  };
-
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    if (!firebase.apps.length) {
-      firebase.initializeApp(Config);
-    }
-  }
-
-  onPress = async () => {
-    try {
-      const result = await Google.logInAsync({
-        androidClientId: '513477020718-j9c5dqpq19e0ejb1vl4g5co085ik97mb.apps.googleusercontent.com',
-        scopes: ['profile', 'email'],
-      });
-
-      if (result.type === 'success') {
-        const { idToken, accessToken } = result;
-        const credential = firebase.auth.GoogleAuthProvider.credential(idToken,accessToken);
-        
-        firebase
-          .auth()
-          .signInAndRetrieveDataWithCredential(credential)
-          .then(res => {
-            console.log("loggedIn:",res)
-            // user res, create your user, do whatever you want
-          })
-          .catch(error => {
-            console.log('firebase cred err:', error.message);
-          });
-      } else {
-        return { cancelled: true };
-      }
-    } catch (err) {
-      console.log('err:', err);
-    }
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={this.onPress}>
-        <Image style={{height: 20, width: 220}}
-          source={require('../../assets/google.png')}/>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+  apiKey: "AIzaSyCflhxjfW0Kf1NG-T51i2LzJd7SSC47BaI",
+  authDomain: "projectexpo-fce16.firebaseapp.com",
+  databaseURL: "https://projectexpo-fce16-default-rtdb.firebaseio.com",
+  projectId: "projectexpo-fce16",
+  storageBucket: "projectexpo-fce16.appspot.com",
+  messagingSenderId: "513477020718",
+  appId: "1:513477020718:web:bee1c1ad733e7b9dbb23bf",
+  measurementId: "G-QR3TBQN02D"
+};
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(Config);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    bottom:90,
-    padding: 8,
-  },
-  button: {
-    marginHorizontal: 90,
-   
-    justifyContent: 'center',
-    
-    
-  },
-  text: {
-    textAlign: 'center',
-  },
-});
+WebBrowser.maybeCompleteAuthSession();
+
+export default function App() {
+
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest(
+    {
+      clientId: '513477020718-9l7r57nku76f7rm16fmbd4st590304ff.apps.googleusercontent.com',
+      },
+  );
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { id_token } = response.params;
+      
+      const credential = firebase.auth.GoogleAuthProvider.credential(id_token);
+      firebase.auth().signInWithCredential(credential);
+    }
+  }, [response]);
+
+  return (
+    <Button
+      disabled={(!request)}
+      title="Login"
+      onPress={() => {
+        promptAsync();
+        }}
+    />
+  );
+}
+
+
+
+
+
